9a0a53472acd1aa0904c613c51621cc1bb34b6bc
eric <phantomenace87@gmail.com>
2026-02-01 16:03:03 -0800

    Initial Commit

---
+++ GuitarIt/GuitarItApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuitarItApp.swift
+//  GuitarIt
+//
+//  Created by Eric Tuesta on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct GuitarItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
