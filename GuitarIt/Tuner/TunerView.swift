a9b6ecab9b4238a97c2b1a925a9f29d0a266132c
eric <phantomenace87@gmail.com>
2026-02-24 09:42:09 -0800

    UI tuner updates (red/yellow/green depending on how close the frequency is)

--- GuitarIt/Tuner/TunerView.swift
+++ GuitarIt/Tuner/TunerView.swift
@@ -4,8 +4,13 @@ import SwiftUI
 struct TunerView: View {
     // Connects to the TunerViewModel
     @StateObject private var viewModel = TunerViewModel()
+    @State private var color: Color = .white
     
-    // TODO: make keyboard go away a function
+    
+    struct TunerState: Equatable {
+        var note: String
+        var cents: Double
+    }
     
     
     var body: some View {
@@ -22,12 +27,27 @@ struct TunerView: View {
                 Text(viewModel.currentNote)
                     .font(.system(size: 200))
                     .fontWeight(.bold)
+                    .foregroundStyle(color)
                 Text("\(viewModel.centsOffset, specifier: "%.1f") cents")
                     .font(.largeTitle)
+                    .foregroundStyle(color)
                 Spacer()
                 HStack {
                     OnOffSwitch(isOn: $viewModel.isOn)
                     TuningStandard(aVal: $viewModel.tuningStandard)
+                }
+            }
+            .onChange(of: TunerState(note: viewModel.currentNote, cents: viewModel.centsOffset)) { old, new in
+                if (viewModel.isOn) {
+                    if (abs(new.cents) > 20.0){
+                        color = .red
+                    } else if (abs(new.cents) > 2.0) {
+                        color = .yellow
+                    } else {
+                        color = .green
+                    }
+                } else {
+                    color = .white
                 }
             }
         }
--- Logbook/Week6.md
+++ Logbook/Week6.md
@@ -1,3 +1,4 @@
 Refactored tuner math into its own class and bound audio capture to the on/off switch in tuner, tested for functionality (~1hr 30 min)
 
 Using MIDI to calculate the closest note and cent offset, updating UI accordingly (~1hr 30 min)
+UI tuner updates (red/yellow/green depending on how close the frequency is) (~30 min)
