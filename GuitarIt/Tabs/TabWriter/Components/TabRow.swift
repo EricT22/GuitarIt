a639b56ce0dd3add6d8b2e043b57ad844e746301
eric <phantomenace87@gmail.com>
2026-04-03 21:06:31 -0700

    .

---
+++ GuitarIt/Tabs/TabWriter/Components/TabRow.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+
+struct TabRow: View {
+    @Binding var tab: TabItem
+    
+    var body: some View {
+        HStack {
+            Text(tab.displayName)
+                .foregroundStyle(Color.secondary)
+            Spacer()
+            Button(action: {
+                withAnimation(.easeInOut){
+                    tab.toggleFavorite()
+                }
+            }, label: {
+                Image(systemName: tab.isFavorite ? "star.fill" : "star")
+                    .foregroundStyle(tab.isFavorite ? Color.yellow : Color.secondary)
+                    .font(.system(size: 20))
+            })
+                .buttonStyle(.borderless)
+        }
+        .contentShape(Rectangle())
+    }
+}
